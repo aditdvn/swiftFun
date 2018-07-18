6016492d0ec03d5bb44d5bbb752490e68dc66a68
Adit Dayal <AditDayal@sachins-macbook.domain_not_set.invalid>
2018-07-18 14:01:49 -0500

    My First Commit as a Gitter!

--- Swift Fun/ViewController.swift
+++ Swift Fun/ViewController.swift
@@ -9,10 +9,89 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+   
+    var buttonCount = 0
+    @IBOutlet weak var myLabel: UILabel!
+    
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+        
+        buttonCount += 1
+        
+        print("You have hit me \(buttonCount) times!")
+        
+        if buttonCount >= 10 {
+        
+        view.backgroundColor = UIColor.red
+        
+        myLabel.text = "You have hit me 10 times!"
+        }
+        if buttonCount >= 20 {
+            
+            view.backgroundColor = UIColor.green
+            
+            myLabel.text = "You have hit me 20 times!"
+        }
+        if buttonCount >= 30 {
+            
+            view.backgroundColor = UIColor.blue
+            
+            myLabel.text = "You have hit me 30 times!"
+        }
+        if buttonCount >= 40 {
+            
+            view.backgroundColor = UIColor.yellow
+            
+            myLabel.text = "You should really stop now..."
+        }
+        if buttonCount >= 50 {
+            
+            view.backgroundColor = UIColor.orange
+            
+            myLabel.text = "I know it's entertaining, but..."
+        }
+        if buttonCount >= 60 {
+            
+            view.backgroundColor = UIColor.magenta
+            
+            myLabel.text = "Okaaayyyyy..."
+        }
+        if buttonCount >= 70 {
+            
+            view.backgroundColor = UIColor.gray
+            
+            myLabel.text = " 3 "
+        }
+        if buttonCount >= 80 {
+            
+            view.backgroundColor = UIColor.purple
+            
+            myLabel.text = " 2 "
+        }
+        if buttonCount >= 90 {
+            
+            view.backgroundColor = UIColor.cyan
+            
+            myLabel.text = " 1 "
+        }
+        if buttonCount >= 100 {
+            
+            view.backgroundColor = UIColor.white
+            
+            myLabel.text = " YOU HAVE NOW HIT ME 100 TIMES! "
+        }
+    }
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+       
     }
 
     override func didReceiveMemoryWarning() {
